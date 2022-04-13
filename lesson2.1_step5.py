2e7ad8ee7dd21e6b997b854317d9b330915b65df
Никита Миронов <nikita34mironov@yandex.ru>
2022-04-13 17:34:34 +0300

    Homework form previous has been added

---
+++ lesson2.1_step5.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+import math
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    import math
+
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    field = browser.find_element_by_id('answer')
+    field.send_keys(y)
+
+    checkbox = browser.find_element_by_id('robotCheckbox')
+    checkbox.click()
+
+    radiobatton = browser.find_element_by_css_selector('[for="robotsRule"]')
+    radiobatton.click()
+
+    button = browser.find_element_by_css_selector('.btn')
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
